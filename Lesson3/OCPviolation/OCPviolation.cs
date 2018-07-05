a6c3e9003ed81835427e79d1ae3001d6db400852
Artiom Nichifor <artiom.nichifor@outlook.com>
2018-07-05 15:27:49 +0300

    created file lesson2

--- Lesson3/OCPviolation/OCPviolation.cs
+++ Lesson3/OCPviolation/OCPviolation.cs
@@ -6,13 +6,56 @@ using System.Threading.Tasks;
 
 namespace OCPviolation
 {
+    public class Animal
+    {
+    }
+
+    public class Dog : Animal
+    {
+    } 
+
+    public class Cat : Animal
+    {
+    }
 
 
     class OCPviolation
     {
+        public void Bark(Animal d)
+        {
+            Console.WriteLine("Bark!");
+        }
+
+        public void Meow(Animal c)
+        {
+            Console.WriteLine("Meow!");
+
+        }
+
+        public void DoNoise(Animal a)
+        {
+            if (a.GetType() == typeof(Dog))
+            {
+                Bark(a);
+            }
+            else if (a.GetType() == typeof(Cat))
+            {
+                Meow(a);
+            }
+           
+
+            
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OCPviolation ocp = new OCPviolation();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            ocp.DoNoise(dog);
+            ocp.DoNoise(cat);
+
             Console.ReadKey();
         }
     }
